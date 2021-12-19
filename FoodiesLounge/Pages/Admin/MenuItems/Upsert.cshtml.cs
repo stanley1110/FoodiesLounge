849d4e7af3c160029f4e4bcc3bd13f8b95b6049c
EStanley <edohemeka2@gmail.com>
2021-12-19 21:05:31 +0100

    File upload features added

--- FoodiesLounge/Pages/Admin/MenuItems/Upsert.cshtml.cs
+++ FoodiesLounge/Pages/Admin/MenuItems/Upsert.cshtml.cs
@@ -14,15 +14,17 @@ namespace FoodiesLounge.Pages.Admin.MenuItems
         private IFoodTypeRepo _db2;
         private ICategoryRepo _db1;
         private IMenuItemRepo _db;
+        private readonly IWebHostEnvironment _webHost;
         public MenuItem  menuItems { get; set;}
         public IEnumerable<SelectListItem>  CategoryList { get; set; }
         public IEnumerable<SelectListItem> FoodTypeList { get; set; }
 
-        public UpsertModel( IMenuItemRepo appDb,ICategoryRepo db1, IFoodTypeRepo db2 )
+        public UpsertModel( IMenuItemRepo appDb,ICategoryRepo db1, IFoodTypeRepo db2, IWebHostEnvironment webHost )
         {
             _db = appDb;
             _db1 = db1;
             _db2 = db2;
+            _webHost = webHost;
             menuItems = new();
         }
         public void OnGet()
@@ -39,6 +41,32 @@ namespace FoodiesLounge.Pages.Admin.MenuItems
                 Value = c.Id.ToString()
 
             });
+        }
+        public async Task<IActionResult> OnPost(MenuItem menuItems ) 
+        {
+            string webRoothPath = _webHost.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            if(menuItems.Id == 0)
+            {
+                string ParenthPath = @"Images\MenuItems";
+                string filename = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(webRoothPath,ParenthPath );
+                var extension = Path.GetExtension(files[0].FileName);    
+                using(var stream = new FileStream(Path.Combine(uploads,filename+extension),FileMode.Create))
+                {
+                    files[0].CopyTo(stream);
+                    menuItems.Image = ParenthPath + filename + extension;
+                    _db.Add(menuItems);
+                    _db.Save();
+                }
+
+            }
+            else
+            {
+
+            }
+            return Page();
+
         }
     }
 }
